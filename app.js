bc6a028dd0c52e1f0885c3a175cd5bf2bf505cc4
Veronika <nebesnav@gmail.com>
2020-06-28 17:56:47 +0300

    add app.js

---
+++ app.js
@@ -0,0 +1,67 @@
+let contMessageBtn = document.querySelector('.contact_message');// Шукаємо кнопку зв'язку
+// console.log(sendMessageBtn);
+
+let modalWind = document.querySelector('.modal');// шукаємо вікно
+// console.log(modalWind);
+
+let closeBtn=document.querySelector('.close');// шукаємо хрестик
+// console.log(closeBtn);
+
+let sendMesBtn = document.querySelector(".send_message")// шукаємо кнопку відправки форми
+// console.log(sendMesBtn) 
+
+contMessageBtn.addEventListener("click",function(){ // показується вікно при натисканні на кнопку зв'язку
+    modalWind.classList.add("show")
+})
+
+closeBtn.addEventListener("click",closeModalWind);// задання події і виклик функції при кліку на хрестик
+sendMesBtn.addEventListener("click",closeModalWind);// і на кнопку відправки форми
+
+function closeModalWind(){
+    modalWind.classList.remove("show") //видаляє клас для якого display:block
+    modalWind.classList.add("hide")//назначає клас для якого display:hide
+}
+
+
+// Закривання вікна при кліку за межами форми
+// modalWind.addEventListener("click", function(e){
+//     let target = e.target
+//     if(target === modalWind){
+//         closeModalWind()
+//     }
+// })
+
+
+let heightWind = document.body.scrollHeight;
+// console.log(heightWind)
+let halfHeight = heightWind/2;
+
+
+let modal_wind_sub = document.querySelector('.modal_wind');
+
+let closeBtnWind = document.querySelector('.btn-close')
+
+function closeModalWindows(){
+    modal_wind_sub.classList.remove('show');
+    modal_wind_sub.classList.add('hide')
+}
+
+closeBtnWind.addEventListener("click",closeModalWindows);
+modal_wind_sub.addEventListener('click',function(e){
+    let target=e.target;
+    if(target == modal_wind_sub){
+        closeModalWindows()
+    }
+})
+
+
+
+window.addEventListener('scroll',displayModal);
+
+
+
+function displayModal (){
+    if(window.pageYOffset > halfHeight){
+    modal_wind_sub.classList.add("show");
+}
+}
